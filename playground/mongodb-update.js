26fab8a585a2fddf80b39b5f4248d4834c638d2e
jprowell <dtroxx@gmail.com>
2018-03-01 15:58:39 -0800

    updates

--- playground/mongodb-update.js
+++ playground/mongodb-update.js
@@ -11,6 +11,8 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
   console.log('Connected to MongoDB server');
   const db = client.db('TodoApp');
 
+ 
+ // Finds user by id and sets completed to true
   db.collection('Todos').findOneAndUpdate({
     _id: new ObjectID('5a9879e394e83c095ce55ccb')
   }, {
@@ -18,11 +20,13 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
       completed: true
     }
   }, {
-    returnOriginal: false
+    returnOriginal: false // returns modified document otherwise original would be returned by default
   }).then((result) => {
     console.log(JSON.stringify(result, undefined, 2));
   });
 
+  
+  // finds user by id and updates name and increments age
   db.collection('Users').findOneAndUpdate({
     _id: new ObjectID('5a9878f38034f2293cb87c86')
   }, {
@@ -33,7 +37,7 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
       age: 1
     }
   }, {
-    returnOriginal: false
+    returnOriginal: false // returns modified document
   }).then((result) => {
     console.log(result);
   });
