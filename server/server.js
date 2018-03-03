1566ea89fca695e3bd28a621c989f182e0aa5956
dtroxx <dtroxx@gmail.com>
2018-03-02 19:29:27 -0800

    Add GET /todos rout

--- server/server.js
+++ server/server.js
@@ -18,6 +18,16 @@ app.post('/todos', (req, res) => {
         res.send(doc);
     }, (e) => {
         res.status(400).send(e);
+    });
+});
+
+app.get('/todos', (req, res) => {
+    Todo.find().then((todos) => {
+        res.send({
+            todos: todos
+        });
+    }, (e) => {
+        res.status(400).send(e);
     })
 });
 
