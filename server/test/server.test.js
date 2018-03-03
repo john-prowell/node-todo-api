9422c482d211252a0ec7dd955edc985dbceaa4b5
dtroxx <dtroxx@gmail.com>
2018-03-02 18:46:22 -0800

    Test POST /todos route

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -40,6 +40,22 @@ describe('POST /todos', () => {
   });
 
   it('should not create todo with a invalid body data', (done) => {
-    
+    request(app)
+      .post('/todos')
+      .send({})
+      .expect(400)
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+
+        Todo.find().then((todos) => {
+          expect(todos.length).toBe(0);
+          done();
+        }).catch((e) => {
+          done(e);
+        });
+      });
+
   });
 });
