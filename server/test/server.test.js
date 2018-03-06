963cb01bbb2010fd8f00c74de05be1b4ac846606
dtroxx <dtroxx@gmail.com>
2018-03-05 20:17:03 -0800

    in progress

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -1,13 +1,16 @@
 const expect = require('expect');
 const request = require('supertest');
 
+const {ObjectID} = require('mongodb');
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
 // Seed todos
 const todos = [{
+  _id: new ObjectID(), // assigns a new Object ID to _id
   text: 'First test todo'
 }, {
+  _id: new ObjectID(),
   text: 'Second test todo'
 }];
 
@@ -80,3 +83,21 @@ describe('GET /todos', () => {
       .end(done);
   });
 });
+
+describe('GET /todos/:id', () => {
+  it('should return todo doc', (done) => {
+    request(app)
+      .get(`/todos/${todos[0]._id.toHexString()}`)
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todo.text).toBe(todos[0].text);
+      })
+      .end(done);
+  });
+
+  it ('should return a 404 if todo not found', (done) => {
+    request(app)
+    .get('/todos/')
+  })
+
+})
