9a038b330aa152362971e62c7924f0ba93c7918c
dtroxx <dtroxx@gmail.com>
2018-03-07 12:42:34 -0800

    Updated mongodb connection

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,8 +1,11 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise; // tells mongoose to use built in Promise libary
-// mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp'); // check heroku mlab mongodb addon or use local machine
-mongoose.connect('mongodb://todoapp:todoapp@ds157818.mlab.com:57818/todoapp')
+// check heroku mlab mongodb addon or use local machine
+// mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+
+// Use mlab account directly or use local machine
+mongoose.connect('mongodb://todoapp:todoapp@ds157818.mlab.com:57818/todoapp' || 'mongodb://localhost:27017/TodoApp')
 
 module.exports = {
   mongoose: mongoose
