06cde179614efd1631d501bf60e0698f39f215b3
dtroxx <dtroxx@gmail.com>
2018-03-09 13:31:42 -0800

    Add PATCH to /todos/:id route

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
 
@@ -63,6 +64,32 @@ app.delete('/todos/:id', (req, res) => {
         res.status(400).send();
     });
 });
+
+app.patch('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    var body = _.pick(req.body, ['text', 'completed']);
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+
+    if (_.isBoolean(body.completed) && body.completed) {
+        body.completedAt = new Date().getTime(); // returns javascript time stamp
+    } else {
+        body.completed = false;
+        body.completedAt = null;
+    }
+
+    Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+        if (!todo) {
+            return res.status(404).send();
+        }
+        res.send({todo});
+    }).catch((e) => {
+        res.status(400).send();
+    })
+
+});
+
 
 app.listen(port, () => {
     console.log(`Started server on port ${port}.`);
