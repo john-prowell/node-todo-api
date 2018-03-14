57272688a6b19c124fecc93288dbbdcebbdb2e09
jprowell <dtroxx@gmail.com>
2018-03-13 20:28:14 -0700

    updates

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -5,8 +5,10 @@ const {ObjectID} = require('mongodb');
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
-const {todos, populateTodos} = require('./seed/seed');
+const {todos, populateTodos, users, populateUsers} = require('./seed/seed');
 
+// clear users from database before each test and put in seed users
+beforeEach(populateUsers);
 // clear todos from database before each test and put in seed todos
 beforeEach(populateTodos);
 
